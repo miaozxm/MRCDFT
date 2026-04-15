888bec039fc997ac56cde0033c9e5488ef638de2
liviler <liviler@163.com>
2026-04-15 20:15:24 +0800

    support for mpi

--- examples/22Ne/run.sh
+++ examples/22Ne/run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash 
 
-export OMP_NUM_THREADS=12
+export OMP_NUM_THREADS=5
+export MKL_NUM_THREADS=5
+export MKL_DYNAMIC=FALSE
+export MKL_THREADING_LAYER=GNU
 
 start_time=$(date +%s)
 echo -e "\033[32m run ...\033[0m"
 
-../../bin/MRCDFT -p 22Ne_para.dat -d 22Ne_b23.dat
+mpirun -np 8 ../../bin/MRCDFT -p 20Ne_para.dat -d 20Ne_b23.dat
 
 echo calculation is finished !
 end_time=$(date +%s)
